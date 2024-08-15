b36c06968383717e3d89f57d24a2ad332e35f07e
Prabesh001 <142589499+Prabesh001@users.noreply.github.com>
2024-08-15 15:48:59 +0545

    uncommenting the commented components

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import './App.css'
-// import Accordian from './Components/Accordian'
-// import RandomColor from './Components/Random-Color'
-// import StarRating from './Components/Star-Rating'
-// import ImageSlider from './Components/Image-Slider'
-// import Dictionary from './Components/Dictionary'
-// import AnimeApi from './Components/AnimeApi'
-// import Layout from './Components/Layout'
-// import GenerateRandomPassword from './Components/GenerateRandomPassword'
+import Accordian from './Components/Accordian'
+import RandomColor from './Components/Random-Color'
+import StarRating from './Components/Star-Rating'
+import ImageSlider from './Components/Image-Slider'
+import Dictionary from './Components/Dictionary'
+import AnimeApi from './Components/AnimeApi'
+import Layout from './Components/Layout'
+import GenerateRandomPassword from './Components/GenerateRandomPassword'
 import Inventory from './Components/Inventory'
 
 function App() {
 
   return (
     <>
-      {/* <Accordian />
+      <Accordian />
       <RandomColor />
       <StarRating noOfStars={5}/>
       { <ImageSlider 
@@ -23,8 +23,8 @@ function App() {
       /> }
       <Dictionary />
       <AnimeApi />
-      <Layout /> */}
-      {/* <GenerateRandomPassword /> */}
+      <Layout />
+      <GenerateRandomPassword />
       <Inventory />
     </>
   )
